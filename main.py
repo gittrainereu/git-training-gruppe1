b8621f9398e0ad5d68ebce4b69ac3c3703027c1b
Jochen Metzger <j.metzger@t3company.de>
2024-12-09 14:13:20 +0100

    main.py improvement

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+# Jo ho ho This is a sample Python script.
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(zname):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi du, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi du, {zname}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('xPyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Neues aus der 18
