58184c369ce0f2cd15051b23d23ce63b0105cfcd
Jochen Metzger <j.metzger@t3company.de>
2024-12-09 14:12:01 +0100

    main.py adjustment

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# Jochen's sample This is a sample Python script.
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(xname):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi du, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi du, {xname}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print_hi('PyCharm rules the world')
+# so
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
