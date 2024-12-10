f87b152a2ba12e3b6d45cd7d0a5465936f8d46d0
Alexandre Knihar <alexandre.knihar@now-gmbh.de>
2024-12-10 15:44:58 +0100

    feature: super wichtige Änderung

--- main.py
+++ main.py
@@ -4,14 +4,14 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(zxname):
+def print_hi_was_geht(zxname):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi Sie rocker, {zxname}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi was geht, {zxname}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm rules the world')
+    print_hi_was_geht('Meister')
 # so
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
