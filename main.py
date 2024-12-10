98d89926ec09fa821c04b7e1c1a96d34b43ddb9e
markus.nagel <markus.nagel@now-gmbh.de>
2024-12-10 15:40:52 +0100

    markuss feature1

--- main.py
+++ main.py
@@ -4,15 +4,16 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(zxname):
+def print_hi(vorname, nachname):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi Sie rocker, {zxname}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Guten Tag, {vorname}, {nachname}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm rules the world')
+    print_hi('PyCharm is cool')
 # so
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 # Neues aus der 18
+# hier gibt es keinen Anhang
