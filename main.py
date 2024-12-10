fd45ec3c6b9402246a8ccbdabe21deb6a67c217a
tetyana.peterburgska <tetyana.peterburgska@now-gmbh.de>
2024-12-10 15:34:32 +0100

    tetyana feature 1

--- main.py
+++ main.py
@@ -4,14 +4,14 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(zxname):
+def print_nokia(lol):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi Sie rocker, {zxname}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi Sie rocker, {lol}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm rules the world')
+    print_nokia('PyCharm rules the world')
 # so
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
