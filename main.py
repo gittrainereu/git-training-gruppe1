45f4d92e9d4ea7948d1e96f3aa7ca39725dc2ab8
markus.nagel <markus.nagel@now-gmbh.de>
2024-12-10 16:05:29 +0100

    quick fix

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(vorname, nachname):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm is cool')
+    print_hi('Py', 'Charm')
 # so
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
