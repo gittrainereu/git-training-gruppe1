2df8dc70aa6462f41f037b681d04d8dec98814df
Jochen Metzger <j.metzger@t3company.de>
2024-12-10 13:27:44 +0100

    changes in main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(zname):
+def print_hi(zxname):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi du rocker, {zname}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi Sie rocker, {zxname}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
