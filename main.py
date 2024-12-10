d32dee21fdb0ee187067bbba27a2ba1a2ae69c57
Jochen Metzger <j.metzger@t3company.de>
2024-12-10 15:35:27 +0100

    feature/jochen1

--- main.py
+++ main.py
@@ -4,15 +4,16 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(zxname):
+def print_hi(vvname):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi Sie rocker, {zxname}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi Sie rocker, {vvname}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm rules the world')
+    print_hi('PyCharm rules or rules to the world')
 # so
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 # Neues aus der 18
+# Was anderes
